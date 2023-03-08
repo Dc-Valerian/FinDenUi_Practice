77acc2475cc9a188df113c491ba85e259337dad4
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-08 23:07:23 +0100

    added the routes

--- src/Components/AllRoutes/AllRoutes.tsx
+++ src/Components/AllRoutes/AllRoutes.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import Login from '../Auth/Login'
+import Register from '../Auth/Register'
+import HomeScreen from '../HomeScreen/HomeScreen'
 
 const AllRoutes = () => {
+    let element =useRoutes ([
+        {
+            path:"/",
+            element:<HomeScreen/>
+        },
+        {
+            path:"/login",
+            element:<Login/>
+        },
+        {
+            path:"/register",
+            element:<Register/>
+        }
+    ])
   return (
     <div>AllRoutes</div>
   )
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <Container>
+            <Wrapper>
+                <Img>
+                <Logo/>
+                </Img>
+
+                <NavHold>
+                    <nav>Home</nav>    
+                    <nav>About</nav>    
+                    <nav>Solution
+                    <Icon></Icon>    
+                    </nav>    
+                    <nav>Pages
+                   <Icon></Icon>
+                   </nav>
+                   <nav>Contact</nav>    
+                </NavHold>                
+            </Wrapper>
+        </Container>
+    </div>
   )
 }
 
 export default Header
+
+const Icon = styled.div``
+
+const NavHold = styled.div``
+
+const Img = styled.div``
+
+const Logo = styled.img``
+
+const Wrapper = styled.div``
+
+const Container = styled.div``
