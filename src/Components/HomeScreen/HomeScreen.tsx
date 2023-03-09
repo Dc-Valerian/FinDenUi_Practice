5ab76754e0e5f2d5e8fd36b9b6a10290293b9b52
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-09 15:36:58 +0100

    added changes

--- src/Components/HomeScreen/HomeScreen.tsx
+++ src/Components/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import GlobalButton from '../Global/GlobalButton'
 import Header from '../Header/Header'
+import Footer from '../Home/Footer/Footer'
+import Hero from '../Home/Hero/Hero'
 
 const HomeScreen = () => {
   return (
     <div>
         <Header/>
-      
+        <Hero/>
+        <Footer/>
     </div>
   )
 }
