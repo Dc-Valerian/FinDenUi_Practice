285586112e432b1f0bd931498fcf0dbdc51ddbc3
Dc-Valerian <valerianpedro03@gmail.com>
2023-03-11 04:31:08 +0100

    update

--- src/Components/Home/Story/Story.tsx
+++ src/Components/Home/Story/Story.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import line from "../../Assets/line.png"
 
 const Story = () => {
   return (
@@ -9,7 +10,9 @@ const Story = () => {
                 <First>
                   <Title>
                     What's Our Story
-                    <Img/>
+                    <Imag>
+                    <Img src={line}/>
+                    </Imag>
                   </Title>
                   <Sub>Our company was founded in 2008. Water Brand bottle is great for drinking, cooking, activities, and <br />
                      even for children. The product is certified in 50 countries.</Sub>
@@ -38,13 +41,27 @@ display: flex;
 justify-content: space-between;
 `
 
-const Sub = styled.div``
+const Sub = styled.div`
+margin: 10px;
+text-align: center;
+color: grey;
+font-size: 13px;
+`
+
+const Imag = styled.div`
+display: flex;
+align-items: center;
+margin: 5px;
+justify-content: center;
+`
 
 const Img = styled.img``
 
 const Title = styled.div`
 font-weight: 800;
 font-size: 48px;
+display: flex;
+flex-direction: column;
 `
 
 const First = styled.div`
